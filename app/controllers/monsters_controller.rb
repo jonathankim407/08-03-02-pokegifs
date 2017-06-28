52f787de78122d4dcac37ae5fe077cccc04ca079
jonathankim407 <jonathankim407@gmail.com>
2017-06-28 17:02:44 -0400

    adding the pokemon info
    monsters_controller.rb
    def index

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,8 +1,14 @@
 class MonstersController < ApplicationController
 
   def index
+    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/pikachu/", followlocation: true)
+    body = JSON.parse(res.body)
     render json: {
-      message: "ok"
+      id: body["id"],
+      name: body["name"],
+      types: body["types"].map do |type|
+        type["type"]["name"]
+      end
     }
   end
 
