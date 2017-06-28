50fded269340a6b1f323821523f78f34d8041262
jonathankim407 <jonathankim407@gmail.com>
2017-06-28 17:45:17 -0400

    adding the gifs
    monsters_controller.rb
    def index => def show

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,14 +1,19 @@
 class MonstersController < ApplicationController
 
-  def index
-    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/pikachu/", followlocation: true)
-    body = JSON.parse(res.body)
+  def show
+    poke_api_response = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/", followlocation: true)
+    poke_body = JSON.parse(poke_api_response.body)
+
+    giphy = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{poke_body["name"]}&rating=g", followlocation: true)
+    giphy_body = JSON.parse(giphy.body)
+
     render json: {
-      id: body["id"],
-      name: body["name"],
-      types: body["types"].map do |type|
+      id: poke_body["id"],
+      name: poke_body["name"],
+      types: poke_body["types"].map do |type|
         type["type"]["name"]
-      end
+      end,
+      gif: giphy_body["data"].first["images"]["original"]["url"]
     }
   end
 
